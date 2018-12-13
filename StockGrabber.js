e2298c5bd4f6b01e217d1e64374588b592f0cfb8
Winchess1 <ikneazevmd@gmail.com>
2018-12-13 12:01:38 -0600

    extracting data was changed
    Change Open -> Number of trades
    Close-> Volume

--- StockGrabber.js
+++ StockGrabber.js
@@ -1,13 +1,13 @@
 
     const rp = require('request-promise');//take APIs +npm request
-    const stockPriceOpen =[];
-    const stockPriceClose =[];
+    const stockPriceNumberOfTrades =[];
+    const stockPriceVolume =[];
     const stockPriceHigh =[];   
     const stockPriceLow =[];
     const stockPriceMinute =[];
     const EventEmitter = require('events');
     const myEmitter = new EventEmitter();
-    const _ = require('underscore');
+    const _ = require('underscore');       
     const allStocks = {
         url: `https://api.iextrading.com/1.0/ref-data/symbols`,
         json:true,
@@ -26,7 +26,7 @@
             for(x in obj){
                 filterStock[x] = obj[x].symbol;
             }      
-            myEmitter.emit('newListener')
+            myEmitter.emit('newListener');
         })
         .catch(function(err){console.log("error")});
 
@@ -34,50 +34,61 @@
         var temp =[];
         var stockPrices = [];
         var combinedStockPrice = [];
-        myEmitter.once('newListener',(event)=>{
-            for (i=0;i<1;i++){
-            var stockLink = ('https://api.iextrading.com/1.0/stock/'+filterStock[i]+'/chart/dynamic');
-          
-            const stockPrice = {
-                url: stockLink,
-                json:true,
-                rejectUnauthorized: false,
-                requestCert: true,
-                agent: false
-                } 
-            
-                rp(stockPrice)
-                .then((data)=>{    
-                  temp = JSON.stringify(data);
-                  StockObj = JSON.parse(temp); 
-                  
-                  for(x in StockObj.data){
-                    stockPriceMinute.push(StockObj.data[x].minute);
-                    stockPriceOpen.push(StockObj.data[x].open);
-                    stockPriceClose.push(StockObj.data[x].close);
-                    stockPriceHigh.push(StockObj.data[x].high);
-                    stockPriceLow.push(StockObj.data[x].low);
-                    
-                }         
-               
-                
-            })
 
-               .catch(function(err){console.log("error")});
-        }
-        
-        
-       
+        myEmitter.once('newListener',(event)=>{
+            for (i=0;i<1;i++){ dataExtractor(i); }    
         });
-        exportise(stockPriceMinute, stockPriceOpen, stockPriceClose, stockPriceHigh, stockPriceLow);
+
+function dataExtractor(i){
+
+    var stockLink = ('https://api.iextrading.com/1.0/stock/'+filterStock[i]+'/chart/dynamic');
+          
+    const stockPrice = {
+        url: stockLink,
+        json:true,
+        rejectUnauthorized: false,
+        requestCert: true,
+        agent: false
+        } 
+    
+        rp(stockPrice)
+        .then((data)=>{    
+          temp = JSON.stringify(data);
+          StockObj = JSON.parse(temp); 
+          
+          for(x in StockObj.data){
+            stockPriceMinute.push(StockObj.data[x].minute);
+            stockPriceNumberOfTrades.push(StockObj.data[x].numberOfTrades);
+            stockPriceVolume.push(StockObj.data[x].volume);
+            stockPriceHigh.push(StockObj.data[x].high);
+            stockPriceLow.push(StockObj.data[x].low);
+            
+        }         
+       
+        
+    })
+
+       .catch(function(err){console.log("error")});
+
+};
+
+
+
+
+
+        exportise(stockPriceMinute, stockPriceNumberOfTrades, stockPriceVolume, stockPriceHigh, stockPriceLow);
 
 
   
-function exportise (minute, open, close, high, low){ 
+function exportise (minute, NumberOfTrades, volume, high, low){ 
     module.exports.minute = minute;
-    module.exports.open = open;
-    module.exports.close = close;
+    module.exports.NumberOfTrades = NumberOfTrades;
+    module.exports.volume = volume;
     module.exports.high = high;
     module.exports.low = low;
-
+    module.exports.allStocks = filterStock;
+  //  module.exports.funcDataExtractor = dataExtractor();
+    
 };
+
+
