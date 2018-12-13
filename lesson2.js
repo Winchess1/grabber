77616574bfb2ad598038fbd19394f21d1a124fce
Winchess1 <ikneazevmd@gmail.com>
2018-12-13 12:03:50 -0600

    New app.get request

--- lesson2.js
+++ lesson2.js
@@ -1,33 +1,39 @@
+
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 
 
 const stockGrabberDock = require("./StockGrabber"); //Data from StockGrabber
 const minute = stockGrabberDock.minute;
-const open = stockGrabberDock.open;
-const close = stockGrabberDock.close;
+const NumberOfTrades = stockGrabberDock.NumberOfTrades;
+const volume = stockGrabberDock.volume;
 const high = stockGrabberDock.high;
 const low = stockGrabberDock.low;
+const allStocks =stockGrabberDock.allStocks;
+//const funcDataExtractor = stockGrabberDock.funcDataExtractor;
 
 
-app.get('/',(req,res)=>{    
+app.get('/',(req,res,next)=>{    
   //  fs.writeFile('stock.log',patrice);   
-  
-    res.render('template',{minute:minute,
-        open:open,
-        close:close,
-        high:high,
-        low:low}); 
+  res.render('main',{allStocks:allStocks,}); 
 
-})
+});
 
-
-
-
+app.get('/stockGrabberDock/:id',(req,res,next)=>{        
+    const id =req.params.id;
+  //  console.log(typeof(funcDataExtractor.funcDataExtractor));
+   // funcDataExtractor.funcDataExtractor(id);    
+          res.render('template',{minute:minute,
+          NumberOfTrades:NumberOfTrades,
+          volume:volume,
+          high:high,
+          low:low}); 
+          
+  });
 
 
 
