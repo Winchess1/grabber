5ec27de24414df2b5ae4c3c9a8dd03e0c90ef317
Alexei Matusevski <ishmalex@gmail.com>
2018-12-25 14:47:14 -0500

    Update lesson2.js

--- lesson2.js
+++ lesson2.js
@@ -5,6 +5,7 @@ console.log('starting application', port);
 const express = require('express');
 const app = express();
 const fs = require('fs');
+var request = require('request');
 
 app.set('view engine','ejs');
 
@@ -63,6 +64,56 @@ app.get('/stockGrabberDock/:id',(req,res)=>{
   });
 
 
+app.get('/key', function (req, res) {
+
+
+  if (typeof req.query.code === "undefined") {
+    res.render('key');
+    return;
+  }
+  var headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  var options = {
+    //see the Authentication API's Post Access Token method for more information
+    url: 'https://api.tdameritrade.com/v1/oauth2/token',
+    method: 'POST',
+    headers: headers,
+    //POST Body params
+    form: {
+      'grant_type': 'authorization_code',
+      'access_type': 'offline',
+      'code': req.query.code, //get the code
+      'client_id': 'AMATUSEVSKI3@AMER.OAUTHAP',
+      'redirect_uri': 'http://localhost:3000/key'
+    }
+  }
+
+  console.log(options);
+
+
+  //Post Access Token request
+  request(options, function (error, response, body) {
+    console.log(response);
+    if (!error && response.statusCode == 200) {
+
+      //see Post Access Token response summary for what authReply contains
+      authReply = JSON.parse(body);
+
+      //the line below is for convenience to test that it's working after authenticating
+      res.render('key', { key: authReply });
+    } else {
+      res.render('key', { error: error, statusCode:response.statusCode, body:body  });
+
+    }
+  })
+
+  function errorHandler(err, req, res, next) {
+    res.status(500)
+    res.render('error', { error: err })
+  }
+});
 
 
 
