d33778705a226a389f5b566aa4ee8e1acd2554e9
Alexei Matusevski <ishmalex@gmail.com>
2018-12-25 23:28:43 -0500

    Update tda.js

--- tda.js
+++ tda.js
@@ -1,67 +1,133 @@
-
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
 var request = require('request');
+const keyFile = 'key.json';
+console.log('\tloading module TDA')
 
-//SSL cert
-var privateKey = fs.readFileSync('/Users/tbd/.ssh/key.pem', 'utf8');
-var certificate = fs.readFileSync('/Users/tbd/.ssh/certificate.pem', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
+// object TDA
+function tda() {
 
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-
-
-app.get('/', function (req, res) {
+    // save this for closure;
+    var that = this;
+    that.accessKey = '';
+    this.periodType = Object.freeze({ day: 'day', month: 'month', year: 'year', ytd: 'ytd' })
 
 
-    if (typeof req.query.code === "undefined") {
-        res.render('main');
-        return;
-    }
-    var headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    /**
+     * Returns the json access token
+     * {
+     *  "access_token": string,
+     *   "refresh_token": string,
+     *   "expires_in": int,
+     *   "refresh_token_expires_in": int,
+     *   "token_type": "Bearer",
+     *   "create_time": int
+     * }
+     */
+    this.getAccessKey = function getAccessKey() {
 
-    var options = {
-        //see the Authentication API's Post Access Token method for more information
-        url: 'https://api.tdameritrade.com/v1/oauth2/token',
-        method: 'POST',
-        headers: headers,
-        //POST Body params
-        form: {
-            'grant_type': 'authorization_code',
-            'access_type': 'offline',
-            'code': req.query.code, //get the code
-            'client_id': 'AMATUSEVSKI@AMER.OAUTHAP',
-            'redirect_uri': 'http://localhost'
+        // if it's not empty - return it
+        if (that.accessKey != '') {
+            console.log('TDA - cached access key')
+            return that.accessKey;
         }
-    }
-
-    //Post Access Token request
-    request(options, function (error, response, body) {
-        console.log(response);
-        if (!error && response.statusCode == 200) {
-
-            //see Post Access Token response summary for what authReply contains
-            authReply = JSON.parse(body);
-
-            //the line below is for convenience to test that it's working after authenticating
-            res.send(authReply);
+        // show saved key if it exists
+        // if file exists
+        if (!fs.existsSync(keyFile)) {
+            console.log('TDA - ' + keyFile + ' file not found')
+            throw 'file ' + keyFile + ' not found';
         }
-    })
-
-    function errorHandler(err, req, res, next) {
-        res.status(500)
-        res.render('error', { error: err })
+        console.log('TDA - read file')
+        that.accessKey = JSON.parse(fs.readFileSync(keyFile));
+        return that.accessKey;
     }
-});
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+    /**
+     * Request access token from TDA
+     * @param {string} code - request code from TDA
+     */
+    this.requestToken = function (code, done) {
+        var headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-//Set to 8080, but can be any port, code will only come over https, even if you specified http in your Redirect URI
-httpServer.listen(8080);
-httpsServer.listen(443);
+        var options = {
+            //see the Authentication API's Post Access Token method for more information
+            url: 'https://api.tdameritrade.com/v1/oauth2/token',
+            method: 'POST',
+            headers: headers,
+            //POST Body params
+            form: {
+                'grant_type': 'authorization_code',
+                'access_type': 'offline',
+                'code': code, //get the code
+                'client_id': 'AMATUSEVSKI3@AMER.OAUTHAP',
+                'redirect_uri': 'http://localhost:3000/key'
+            }
+        }
+
+        console.log('TDA requesting token');
+
+        // Post Access Token request
+        request(options, function (error, response, body) {
+            console.log('TDA - code', response.statusCode);
+
+            if (!error && response.statusCode == 200) {
+
+                //see Post Access Token response summary for what authReply contains
+                authReply = JSON.parse(body);
+
+                // add to current time
+                authReply.create_time = + new Date();
+
+                // save key to file
+                fs.writeFile('key.json', JSON.stringify(authReply), function (err) {
+                    if (err) throw err;
+                    console.log('New Key Saved');
+                });
+
+                done(null, response.statusCode, authReply);
+            } else {
+                done({ error, body, statusCode: response.statusCode }, null);
+            }
+        })
+    }
+
+
+    /**
+     * get price history
+     * @param {string} symbol - stock symbol
+     * @param {tda.periodType} periodType The type of period to show. Valid values are _day_, _month_, _year_, or _ytd_ (year to date). Default is day.
+     * @param {*} period - day: 1, 2, 3, 4, 5, 10*; month: 1*, 2, 3, 6; year: 1*, 2, 3, 5, 10, 15, 20 ytd: 1*
+     * @param {*} frequency - minute: 1*, 5, 10, 15, 30; daily: 1*; weekly: 1*; monthly: 1*
+     * @param {*} frequencyType - day: minute*; month: daily, weekly*; year: daily, weekly, monthly*; ytd: daily, weekly*;
+     * @param {*} needExtendedHoursData - true | false
+     */
+    this.getPriceHistory = function (symbol, periodType, period, frequency, frequencyType, needExtendedHoursData, done) {
+        // function get_tda_price(res, stock, done) {
+        var headers = {
+            'Authorization': that.getAccessKey()
+        }
+
+        var options = {
+            //see the Authentication API's Post Access Token method for more information
+            url: `https://api.tdameritrade.com/v1/marketdata/${symbol}/pricehistory?apikey=AMATUSEVSKI3&periodType=${periodType}&period=${period}&frequencyType=${frequencyType}&frequency=${frequency}&needExtendedHoursData=${needExtendedHoursData}`,
+            method: 'GET',
+            headers: headers
+        }
+
+        request(options, function (error, response, body) {
+            console.log(`TDA - get prices for ${symbol} HTTP status ${response.statusCode}`);
+            if (!error && response.statusCode == 200) {
+                done(null, JSON.parse(body))
+            } else {
+                done(error, body);
+            }
+        })
+    }
+
+    return this;
+}
+
+// return instance of TDA object
+module.exports = tda();
+console.log('\tloading module TDA END')
